eff03ab9f6b8e6107c994d70485934626b32ce5e
TVcraft01 <tvcraft06@gmail.com>
2026-09-17 00:32:34 +0200

    Fix download-eval evasion detection direction

--- bioaegis/host_scanner.py
+++ bioaegis/host_scanner.py
@@ -27,7 +27,7 @@ EICAR_TEST_SIGNATURE = b"X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-T
 
 SUSPICIOUS_PATTERNS = (
     ("download-and-execute", re.compile(rb"(?:curl|wget)[^\n]{0,300}(?:\||;)[^\n]{0,100}(?:sh|bash)")),
-    ("download-eval", re.compile(rb"(?:curl|wget)[^\n]{0,300}(?:\$\(|<\()[^\n]{0,120}(?:eval|source|exec)")),
+    ("download-eval", re.compile(rb"(?:eval|source|exec)[ \t\r\n]*(?:[\"']?\$\([ \t\r\n]*)?(?:curl|wget)[^\n]{0,300}\)?")),
     ("base64-payload", re.compile(rb"base64[ \t]+(?:-d|--decode)")),
     ("reverse-shell", re.compile(rb"(?:/dev/tcp/|nc[ \t]+[^\n]{0,80}-e[ \t]+(?:/bin/)?(?:sh|bash))")),
     ("destructive-command", re.compile(rb"(?:rm[ \t]+-rf[ \t]+/|mkfs\.|dd[ \t]+if=/dev/(?:zero|random))")),
